0054dd32f6368c69d4284fba617b4b819ee3c433
EmeritusK <jparedes1270@gmail.com>
2025-09-28 13:41:18 -0500

    Add Tailwind CSS configuration and custom font styles; update layout to include global styles and adjust asset build path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+    build: {
+        assets: 'assets'
+    }
 });
